86fc562bf10ce62c5109a6530b8572aab0498f84
sharo-jef <sharo.jef@gmail.com>
2024-02-21 01:12:30 +0900

    fix

--- src/server.ts
+++ src/server.ts
@@ -4,19 +4,14 @@ import { config } from './lib/config';
 import { Logger } from './lib/logger';
 import { Router } from './controller/router';
 
-const app = express();
-
-(async () => {
+try {
+  const app = express();
   const router = new Router(app);
   router.register();
   app.listen(config.PORT, () =>
     Logger.info(`⚡Server is running on port ${config.PORT}`)
   );
-})()
-  .catch((e) => {
-    Logger.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    // Do some cleanup (e.g. close database connections, etc.)
-  });
+} catch (e) {
+  Logger.error(e);
+  process.exit(1);
+}
